077ca56fd69476012ddb73a2fab18d14f7a23a14
sDaCoder <supravadutta_sDa@outlook.com>
2023-09-03 22:17:43 +0530

    Port Changed

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const path = require("path");
 const fs = require("fs");
 const app = express();
-const port = 8080;
+const port = 80;
 
 // EXPRESS SPECIFIC STUFF
 app.use('/static', express.static('static')) // For serving static files
